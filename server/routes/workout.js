a39f9fac31d4cc562c01c973ab76b345851e6196
SarveshJoshi25 <contact.sarveshjoshi@gmail.com>
2023-02-06 00:17:55 +0530

    Lecture 5: Databases are set up

--- server/routes/workout.js
+++ server/routes/workout.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+const Workout = require('../models/workoutModel')
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -15,10 +16,18 @@ router.get('/:id', (req, res) => {
 })
 
 
-router.post('/', (req, res) => {
-    res.status(200).json({
-        message: `You've just requested to get Workout (POST)`
-    })
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+
+    try{ 
+        const workout = await Workout.create({title, load, reps})
+        res.status(201).json({message : `Workout: ${workout.title} created successfully!`})
+    }catch (error) {
+        res.status(400).json({
+            error: error
+        })
+    }
+    
 })
 
 router.delete('/:id', (req, res) => {
